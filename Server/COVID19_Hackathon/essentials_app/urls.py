37a1638c6aa797d49cc13408d3ba4aa64a029f44
AAISHWARYA <aaishwaryadmk@gmail.com>
2020-03-29 00:18:48 +0400

    minor changes

--- Server/COVID19_Hackathon/essentials_app/urls.py
+++ Server/COVID19_Hackathon/essentials_app/urls.py
@@ -1,8 +1,14 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
 from . import views
 
-urlpatterns = [
-    path('', views.insertData, name='home-page')
-]
+router = routers.DefaultRouter()
+router.register(r'places', views.matchData)
 
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
+urlpatterns = [
+    path('', include(router.urls)),
+    path('', views.matchData, name='home-page')
+]
 
